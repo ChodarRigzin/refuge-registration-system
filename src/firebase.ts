ac75702ddbd17a6a97167acc737ec2a9d2722c69
ChodarRigzin <faxin311@gmail.com>
2025-06-11 21:14:21 +0800

    chore: Trigger redeploy

--- src/firebase.ts
+++ src/firebase.ts
@@ -12,7 +12,6 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  // 這裡補上了前一行缺少的逗號
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID, 
 };
 
